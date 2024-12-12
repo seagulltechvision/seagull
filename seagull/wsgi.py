e8613f3f669707a10c424fcaf66cd038d37d8a6e
seagulltechvision <seagulltechhr@gmail.com>
2024-12-12 14:36:00 +0530

    final-commit-chnges

--- seagull/wsgi.py
+++ seagull/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+print("WSGI application initialized")
 import os
 from django.core.wsgi import get_wsgi_application
 
@@ -15,3 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seagull.settings')
 application = get_wsgi_application()
 # Vercel expects a handler for serverless functions
 handler = application
+print("WSGI application initialized")
